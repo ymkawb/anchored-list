17df55937654c4fbb7441052fe4f70bd11217dc1
Nikolay Ivanov <nik.ivanov@gmail.com>
2018-03-11 17:11:01 +0300

    Sealed class -> Enum

--- app/src/main/java/com/example/nivanov/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/nivanov/myapplication/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.nivanov.myapplication
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.util.DiffUtil
 import android.support.v7.widget.DefaultItemAnimator
@@ -16,29 +15,59 @@ import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.function.BiFunction
 
-sealed class ScrollState {
-    abstract fun nextState(nextScroll : Int, layoutManager: LinearLayoutManager ) : ScrollState
-    abstract fun shouldScroll() : Boolean
-}
+enum class ScrollState {
+    Init {
+        override fun <T : RecyclerView.ViewHolder> nextState(nextScroll: Int, layoutManager: LinearLayoutManager, adapter: RecyclerView.Adapter<T>): ScrollState = when (nextScroll) {
+            RecyclerView.SCROLL_STATE_DRAGGING -> Dragging
+            else -> this
+        }
+        override fun shouldScroll(layoutManager: LinearLayoutManager): Boolean = true
+    },
+    Dragging {
+        override fun <T : RecyclerView.ViewHolder> nextState(nextScroll: Int, layoutManager: LinearLayoutManager, adapter: RecyclerView.Adapter<T>): ScrollState = when (nextScroll) {
+            RecyclerView.SCROLL_STATE_SETTLING -> DraggingSettling
+            RecyclerView.SCROLL_STATE_IDLE ->
+                if (layoutManager.findLastVisibleItemPosition() == adapter.itemCount - 1) Init else ScrollLocked
+            else -> this
+        }
 
-class Init : ScrollState() {
-    override fun nextState(nextScroll: Int, layoutManager: LinearLayoutManager): ScrollState = this
-    override fun shouldScroll(): Boolean = true
-}
+        override fun shouldScroll(layoutManager: LinearLayoutManager): Boolean = false
+    },
+    ScrollLocked {
+        override fun <T : RecyclerView.ViewHolder> nextState(nextScroll: Int, layoutManager: LinearLayoutManager, adapter: RecyclerView.Adapter<T>): ScrollState = when (nextScroll) {
+            RecyclerView.SCROLL_STATE_DRAGGING -> Dragging
+            else -> this
+        }
+
+        override fun shouldScroll(layoutManager: LinearLayoutManager): Boolean = false
+    },
+    DraggingSettling {
+        override fun shouldScroll(layoutManager: LinearLayoutManager): Boolean = false
+        override fun <T : RecyclerView.ViewHolder> nextState(nextScroll: Int, layoutManager: LinearLayoutManager, adapter: RecyclerView.Adapter<T>): ScrollState = when (nextScroll) {
+            RecyclerView.SCROLL_STATE_IDLE ->
+                if (layoutManager.findLastVisibleItemPosition() == adapter.itemCount - 1) Init else ScrollLocked
+            RecyclerView.SCROLL_STATE_DRAGGING -> Dragging
+            else -> this
+        }
+    };
+
+
+    abstract fun <T : RecyclerView.ViewHolder> nextState(nextScroll: Int, layoutManager: LinearLayoutManager, adapter: RecyclerView.Adapter<T>): ScrollState
+    abstract fun shouldScroll(layoutManager: LinearLayoutManager): Boolean;
+
+};
+
+
 class MainActivity : AppCompatActivity() {
 
     lateinit var list: RecyclerView
     lateinit var repository: Repository
-    var scrollState : ScrollState = Init()
+    var scrollState: ScrollState = ScrollState.Init
     var lastList: List<ViewModel> = emptyList()
-    val zipper: BiFunction<List<ViewModel>, DiffUtil.DiffResult, ViewModelUpdate> = BiFunction { p0, p1 ->
-        Pair(p0, p1)
-    }
 
-    fun log(msg : () -> String) {
-        Log.i("MainActivity",msg())
+    fun log(msg: () -> String) {
+        Log.i("MainActivity", msg())
     }
-
 
 
     private val MILLISECONDS_PER_INCH = 250f
@@ -58,7 +87,7 @@ class MainActivity : AppCompatActivity() {
                 return MILLISECONDS_PER_INCH / displayMetrics!!.densityDpi;
             }
         }
-        val linearLayoutManager = object : LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false){
+        val linearLayoutManager = object : LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false) {
             override fun smoothScrollToPosition(recyclerView: RecyclerView?, state: RecyclerView.State?, position: Int) {
                 scroller.targetPosition = position
                 startSmoothScroll(scroller)
@@ -70,29 +99,30 @@ class MainActivity : AppCompatActivity() {
         list.itemAnimator = object : DefaultItemAnimator() {
             override fun animateAdd(holder: RecyclerView.ViewHolder?): Boolean = false
         }
-        list.addOnScrollListener(object : RecyclerView.OnScrollListener(){
-            override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
-                log{"onScrollStateChanged[newState=$newState]"}
-            }
-        })
+
         repository = Repository()
         val payload: Observable<ViewModelUpdate> = repository.viewModel
                 .subscribeOn(Schedulers.computation())
-                .map {
-                    log { "Calcing diff on ${Thread.currentThread().name}"}
-                    val calculateDiff: DiffUtil.DiffResult = DiffUtil.calculateDiff(DiffCallback(lastList, it))
-                    Pair(it, calculateDiff)
-                }.doAfterNext {
-            val oldSize = lastList.size
-            lastList = it.first
-            val lastPosition = lastList.size
-            list.post{
-                log{"scroll to position $lastPosition"}
-                list.smoothScrollToPosition(lastPosition)
+                .map { Pair(it, DiffUtil.calculateDiff(DiffCallback(lastList, it))) }
+                .doAfterNext {
+                    lastList = it.first
+                    val lastPosition = lastList.size
+                    list.post {
+                        if (scrollState.shouldScroll(linearLayoutManager)) {
+                            log { "scroll to position $lastPosition" }
+                            list.smoothScrollToPosition(lastPosition)
+                        }
+                    }
+                }
+        val adapter = ElementsAdapter(layoutInflater, payload)
+        list.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
+                val oldState = scrollState
+                scrollState = scrollState.nextState(newState, linearLayoutManager, adapter)
+                log { "onScrollStateChanged[newState=$newState, scrollState=$oldState -> $scrollState]" }
             }
-        }
-        val adapter = ElementsAdapter(layoutInflater,payload)
+        })
         list.adapter = adapter
+
     }
 }
